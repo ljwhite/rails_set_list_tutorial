460369dd0321a38520d00bc0358ad4becfe3904b
Lito White <leland.white@gmail.com>
2023-09-26 14:53:05 -0600

    added seeds to dev db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+@prince = Artist.create!(name: 'Prince')
+@purple = @prince.songs.create!(title: 'Purple Rain', length: 15, play_count: 20)
+@beret = @prince.songs.create!(title: 'Raspberry Beret', length: 39, play_count: 100)
+@carly = Artist.create!(name: 'Carly Rae')
+@callme = @carly.songs.create!(title: "Call Me Maybe", length: 200, play_count: 2000)
