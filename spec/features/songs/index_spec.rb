1c7b856ca7f93e5a96a91d60ce8e516e8d4fcbc4
Lito White <leland.white@gmail.com>
2023-09-26 14:50:31 -0600

    artist song page index complete

---
+++ spec/features/songs/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'songs index page' do
+  before(:each) do
+    @prince = Artist.create!(name: 'Prince')
+    @purple = @prince.songs.create!(title: 'Purple Rain', length: 15, play_count: 20)
+    @beret = @prince.songs.create!(title: 'Raspberry Beret', length: 39, play_count: 100)
+    @carly = Artist.create!(name: 'Carly Rae')
+    @callme = @carly.songs.create!(title: "Call Me Maybe", length: 200, play_count: 2000)
+  end
+
+  it 'lists all songs' do
+    visit "/songs/"
+    expect(page).to have_content(@purple.title)
+    expect(page).to have_content(@beret.title)
+    expect(page).to have_content(@callme.title)
+  end
+
+end
